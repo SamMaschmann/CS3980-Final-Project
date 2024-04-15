7ff0be5130cd68a8ac237e4996ad3960d662d4a5
jrenning <jack-renning@uiowa.edu>
2024-04-14 22:59:08 -0500

    Added settings code from class.

---
+++ db.py
@@ -0,0 +1,20 @@
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+from dataModels import Transaction
+
+
+async def init_db():
+    # This will not work until you add a file named .env and put the database secrets in it
+    
+    # sample env file
+    
+    #
+    #
+    # DB_CONN = "this_is_a_url_to_the_db"
+    
+    # Don't add .env file to git
+    conn = os.getenv("DB_CONN")
+    client = AsyncIOMotorClient(conn)
+    await init_beanie(database=client.get_default_database(), document_models=[Transaction])
