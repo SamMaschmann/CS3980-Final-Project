9c71c2dd64ed59019a9c80e077bca5985d244020
jrenning <jack-renning@uiowa.edu>
2024-05-02 11:58:45 -0500

    Add back in add form.

--- frontend/src/routes/budget/budget.tsx
+++ frontend/src/routes/budget/budget.tsx
@@ -12,6 +12,77 @@ export type Expense = {
   amount: number;
 };
 
+const ExpenseForm: React.FC<{
+  addExpense: (newExpense: Expense) => void;
+}> = ({ addExpense }) => {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!name || !category || amount <= 0) return;
+
+    await axios.post(
+      `http://localhost:8000/budgets?token=${localStorage.getItem("token")}`,
+      {
+        name,
+        category,
+        amount,
+      }
+    );
+
+    window.location.reload();
+
+    setName("");
+    setCategory("");
+    setAmount(0);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="expense-form">
+      <h2>Add Expense</h2>
+      <div className="expense-form-pair">
+        <label>Name:</label>
+        <div className="expense-form-input">
+          <input
+            className="input"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="expense-form-pair">
+        <label>Category:</label>
+        <div className="expense-form-input">
+          <input
+            type="text"
+            className="input"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="expense-form-pair">
+        <label>Amount:</label>
+        <div className="expense-form-input">
+          <input
+            type="number"
+            className="input"
+            value={amount}
+            onChange={(e) => setAmount(parseFloat(e.target.value))}
+          />
+        </div>
+      </div>
+      <button type="submit" className="form-button">
+        Add Expense
+      </button>
+    </form>
+  );
+};
+
+
 function Budget() {
   const [expenses, setExpenses] = useState<Expense[]>([]);
 
@@ -59,7 +130,9 @@ function Budget() {
   return (
     <div className="budget-container">
       <h1>Budget Management</h1>
-      <div className="budget-content">
+      <div className="budget-content"></div>
+      <div className="budget-add">
+        <ExpenseForm addExpense={addExpense} />
         <div className="expense-list">
           <h2>Expenses</h2>
           <button className="download-text" onClick={downloadExpenses}>
@@ -71,11 +144,15 @@ function Budget() {
                 {...expense}
                 key={expense.id}
                 onDelete={() => deleteExpense(expense.id)}
-                onEdit={(editedExpense: Expense) => editExpense(expense.id, editedExpense)}
+                onEdit={(editedExpense: Expense) =>
+                  editExpense(expense.id, editedExpense)
+                }
               />
             ))}
           </ul>
         </div>
+      </div>
+      <div>
         <BudgetPie expenses={expenses} />
       </div>
     </div>
